afea57e92f3d0939c08fd3dccd51ca575f1334e7
AV-Master73 <85580482+AV-Master73@users.noreply.github.com>
2023-05-06 13:41:17 +0300

    Update script.js
    Debug element added

--- js/script.js
+++ js/script.js
@@ -115,7 +115,7 @@ $(function() { // Same as document.addEventListener("DOMContentLoaded"...
                 //
                 var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml,
                     "randomCategoryShortName", "'" + chosenCategoryShortName.short_name + "'");
-
+                homeHtmlToInsertIntoMainPage = '<span TEST TEST TEST </span>';
                 // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
                 // Use the existing insertHtml function for that purpose. Look through this code for an example
                 // of how to do that.
