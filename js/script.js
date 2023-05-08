3ded3565f1938f6da3a03da7c677f03cefb253a4
AV-Master73 <85580482+AV-Master73@users.noreply.github.com>
2023-05-08 18:35:10 +0300

    Update script.js
    Supposedly done

--- js/script.js
+++ js/script.js
@@ -82,7 +82,10 @@ $(function() { // Same as document.addEventListener("DOMContentLoaded"...
         // On first load, show home view
         showLoading("#main-content");
         $ajaxUtils.sendGetRequest(
-            allCategoriesUrl, buildAndShowHomeHTML, // ***** <---- TODO: STEP 1: Substitute [...] ******
+            allCategoriesUrl,
+            function(request) {
+                buildAndShowHomeHTML(request.responseText);
+            }, // ***** <---- TODO: STEP 1: Substitute [...] ******
             true); // Explicitly setting the flag to get JSON from server processed into an object literal
     });
     // *** finish **
@@ -99,7 +102,7 @@ $(function() { // Same as document.addEventListener("DOMContentLoaded"...
                 // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
                 // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
                 // variable's name implies it expects.
-                var chosenCategoryShortName = chooseRandomCategory(categories);
+                var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
 
 
                 // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
@@ -114,13 +117,13 @@ $(function() { // Same as document.addEventListener("DOMContentLoaded"...
                 // it into the home html snippet.
                 //
                 var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml,
-                    "randomCategoryShortName", "'" + chosenCategoryShortName.short_name + "'");
-                homeHtmlToInsertIntoMainPage = '<span TEST TEST TEST </span>';
+                    "randomCategoryShortName", "'" + chosenCategoryShortName + "'");
+                //homeHtmlToInsertIntoMainPage = '<span TEST TEST TEST </span>';
                 // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
                 // Use the existing insertHtml function for that purpose. Look through this code for an example
                 // of how to do that.
                 // ....
-                insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
+                insertHtml(document.querySelector("#main-content"), homeHtmlToInsertIntoMainPage);
 
             },
             false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
