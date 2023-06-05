47aa925797892be892ee0b08fafba41219567f2e
AV-Master73 <85580482+AV-Master73@users.noreply.github.com>
2023-06-05 10:39:51 +0300

    Public urls updated

--- js/script.js
+++ js/script.js
@@ -59,7 +59,14 @@ $(function() { // Same as document.addEventListener("DOMContentLoaded"...
     };
 
     // On page load (before images or CSS)
-    document.addEventListener("DOMContentLoaded", function(event) {
+    document.addEventListener("DOMContentLoaded", function (event) {
+        // Instead of Heroku URLs to retrieve the data for the David Chu's China Bistro restraurant website, 
+        // which we build at the end of the course, we are now using Google Firebase.
+        // The new URL is https://coursera-jhu-default-rtdb.firebaseio.com/categories.json 
+        // for retrieving all categories of items and 
+        // https://coursera-jhu-default-rtdb.firebaseio.com/menu_items/{category_short_name}.json 
+        // for retrieving all items for a single category
+        // (e.g., https://coursera-jhu-default-rtdb.firebaseio.com/menu_items/L.json for all items for the L Lunch category).
 
         // TODO: STEP 0: Look over the code from
         // *** start ***
